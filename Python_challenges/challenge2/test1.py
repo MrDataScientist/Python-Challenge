01301c3ae1409a55fdbc76da13f757c0596f32ce
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-30 13:32:06 +0100

    Update test1.py

--- Python_challenges/challenge2/test1.py
+++ Python_challenges/challenge2/test1.py
@@ -4,3 +4,28 @@ for z in string:
   if ord(z)<=90 and ord(z)>=65 or ord(z)<=122 and ord(z)>=97: 
     s+=z 
     print(s);
+
+    
+    
+    #----------------------
+#in python 3 : from urllib.request
+import urllib.request
+
+req = urllib.request.Request("http://www.pythonchallenge.com/pc/def/ocr.html")
+req.add_header('Referer', 'http://www.python.org/')
+# Customize the default User-Agent header value:
+req.add_header('User-Agent', 'urllib-example/0.1 (Contact: . . .)')
+page = urllib.request.urlopen(req)
+
+#page = opener.open("http://www.pythonchallenge.com/pc/def/ocr.html");
+contents = page.read();
+page.close();
+mess = contents.split("<!--")[1].split("-->\n")[1]
+
+items = {};
+
+for  item in mess:
+	if item in items:
+		items[item] = items[item] +1;
+	else:
+		items[item] = 1;
