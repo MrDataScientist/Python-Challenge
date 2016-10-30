cca7d057406dfa95daacb5b619bf66c8df43f9e5
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-30 20:13:23 +0100

    Update Problem-3-List-less-than-ten.py

--- Problem-3-List-less-than-ten.py
+++ Problem-3-List-less-than-ten.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # Problem 3 :List Less Than Ten
@@ -17,9 +16,3 @@ list_number = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for item in list_number:
     if item < 10 :
         print (item)
-
-
-# In[ ]:
-
-
-
