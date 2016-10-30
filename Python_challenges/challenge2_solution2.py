bee340f9eeb2ff23f25760b88a2d02446e820112
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-30 13:01:49 +0100

    Update challenge2_solution2.py

--- Python_challenges/challenge2_solution2.py
+++ Python_challenges/challenge2_solution2.py
@@ -1,6 +1,6 @@
 from urllib import *
 
-page 0 urlopen("");
+page 0 urlopen("http://www.pythonchallenge.com/pc/def/ocr.html");
 contents = page.read();
 page.close();
 print contents.split("--")[1].split
